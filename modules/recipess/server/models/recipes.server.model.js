b56914ff0a45415d764135ecba40a77b40427c7d
DingusBungus <conor.doherty@ufl.edu>
2016-02-21 23:53:52 -0500

    Updated Recipe Schema
    Let me know if you think I should change anything up. For the moment it
    holds both the original recipe and the “nutrified” recipe so we can
    have access to both. I wasn’t sure if this would be necessary or not.

--- modules/recipess/server/models/recipes.server.model.js
+++ modules/recipess/server/models/recipes.server.model.js
@@ -1,34 +1,50 @@
 'use strict';
 
-/**
- * Module dependencies.
- */
-var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
+/* Import mongoose and define any variables needed to create the schema */
+var mongoose = require('mongoose'), 
+    Schema = mongoose.Schema;
 
-/**
- * Recipes Schema
- */
-var RecipesSchema = new Schema({
-  created: {
-    type: Date,
-    default: Date.now
+//I figure storing the original and "nutrified" recipes may be nice
+//BTW: [String] just means an array of Strings
+var recipeSchema = new Schema({
+
+  //ingredients
+  original_ingredients: [String],
+  nutrified_ingredients: [String],
+
+  //tags for allergies/health concerns. This should allow for eacy filtering
+  tags: {
+    allergies: [String],
+    health_concerns: [String]
   },
-  title: {
-    type: String,
-    default: '',
-    trim: true,
-    required: 'Title cannot be blank'
-  },
-  content: {
-    type: String,
-    default: '',
-    trim: true
-  },
-  user: {
-    type: Schema.ObjectId,
-    ref: 'User'
-  }
+  
+  //The steps for cooking
+  directions: String,
+
+  //book-keeping
+  updated_at: Date,
+  created_at: Date
 });
 
-mongoose.model('Recipes', RecipesSchema);
+/* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
+recipeSchema.pre('save', function(next) {
+
+  var currentDate = new Date();
+
+  //if hasn't been created yet, set the date
+  if (!this.created_at) {
+    this.created_at = currentDate;
+  }
+
+  //always update this field
+  this.updated_at = currentDate;
+
+  //move to after the middleware
+  next();
+});
+
+/* Use your schema to instantiate a Mongoose model */
+var Recipe = mongoose.model('Recipe', recipeSchema);
+
+/* Export the model to make it avaiable to other parts of your Node application */
+module.exports = Recipe;
