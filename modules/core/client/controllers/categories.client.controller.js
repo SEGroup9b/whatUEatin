96afff598cd2afd3e15d36bc05f849ffd8755b7c
DingusBungus <conor.doherty@ufl.edu>
2016-03-23 00:08:18 -0400

    Broken version of filtering

--- modules/core/client/controllers/categories.client.controller.js
+++ modules/core/client/controllers/categories.client.controller.js
@@ -4,16 +4,26 @@ angular.module('core').controller('CategoriesController', ['$scope', '$statePara
   function ($scope, $stateParams, Authentication, Recipes) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
-    $scope.tag = 'gluten';
-    $scope.adjustTag = function(newTag) {
-      $scope.tag = newTag;
-      console.log('Adjusting tag to ' + newTag);
+    $scope.filter = '';
+    $scope.adjustFilter = function(newTag) {
+      $scope.filter = newTag.tag;
+      console.log('Adjusting tag to ' + newTag.tag);
+    };
+    $scope.contains = function(index) {
+      var filteredRecipes = [];
+      for (var i = Recipes.length - 1; i >= 0; i--) {
+        if (!Recipes[i].tags.allergies.contains($scope.categories[index].tag) && !Recipes[i].tags.health_concerns.contains($scope.categories[index].tag)) {
+          filteredRecipes.push(Recipes[i]);
+        }
+      }
+      return filteredRecipes;
     };
     $scope.categories = [
-      'Gluten Free',
-      'Low Carb',
-      'Low Fat',
-      'Test'
+    	{ name: 'All', tag: ''},
+      { name: 'Gluten Free', tag: 'gluten' },
+      { name: 'Lactose Free', tag: 'lactose' },
+      { name: 'Low Carb', tag: 'carb' },
+      { name: 'Low Fat', tag: 'fat' }
     ];
     // Find a list of recipes
     $scope.find = function () {
